5d96bcdb92a599b15c1da70e2ed1729fed28c42b
Oytun <oytunozdemir@gmail.com>
2018-05-10 14:11:33 +0300

    initialize

---
+++ database/migrations/2018_05_10_103556_create_log_entries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("type")->nullable(true);
+            $table->string("entry")->nullable(true);
+            $table->integer("logs_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_entries');
+    }
+}
