67292562c589ae1c3db3758b2d18c860aa1886fb
Oytun <oytunozdemir@gmail.com>
2018-05-11 09:07:44 +0300

    login redirect fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/logs';
 
     /**
      * Create a new controller instance.
