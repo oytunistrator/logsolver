752fc9d2334ed93b82bdba08ac32e8e18ee926cd
Oytun <oytunozdemir@gmail.com>
2018-05-14 13:01:14 +0300

    update

--- public/js/app.js
+++ public/js/app.js
@@ -29023,7 +29023,7 @@ if ($(".logs").length > 0) {
     }, { "data": "filename" }, { "data": "created_at" }, {
         "targets": 7,
         "render": function render(data, type, row, meta) {
-            return '<a class="btn" href="' + window.location.href + '/delete/' + row.id + '">Delete</a> <a class="btn" href="' + window.location.href + '/view/' + row.id + '">View</a>';
+            return '<a class="btn deleteObject" href="' + window.location.href + '/delete/' + row.id + '" >Delete</a> <a class="btn" href="' + window.location.href + '/view/' + row.id + '" target="_blank">View</a>';
         }
     }], {
         "url": window.location.href + "/data",
@@ -29100,6 +29100,20 @@ $(".cleanDbAjax").on('click', function (event) {
     var ask = confirm("<b class=\"text-danger\">Cleaning functions are too dangerous! <br> Are you sure to clean?</b>").then(function () {
         $.ajax({
             "url": "/admin/clear/" + option,
+            "type": 'POST',
+            "dataType": 'json',
+            "success": function success(data) {
+                document.location.reload(true);
+            }
+        });
+    });
+});
+
+$(".deleteObj").on('click', function (event) {
+    var url = $(event.target).attr('href');
+    var ask = confirm("<b class=\"text-danger\">Are you sure to delete?</b>").then(function () {
+        $.ajax({
+            "url": url,
             "type": 'POST',
             "dataType": 'json',
             "success": function success(data) {
