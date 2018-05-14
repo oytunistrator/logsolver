ffa64dca7053cbd73143da657d9f4438ae505b43
Oytun <oytunozdemir@gmail.com>
2018-05-14 13:53:26 +0300

    updates

--- resources/views/logfiles/uploader.blade.php
+++ resources/views/logfiles/uploader.blade.php
@@ -29,7 +29,7 @@
                             
                             <div class="form-group">
                                 <div class="progress">
-                                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
                                 </div>
                             </div>
                         </fieldset>
